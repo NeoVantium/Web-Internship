0cf31a3fe270d9ba632876210be6afa69323ae4a
Apoorve Goyal <apoorve73@gmail.com>
2020-09-19 16:10:35 +0530

    REmoved scent

--- scent/js_files/interface/plugins/basket/js/checkout_signin.js
+++
@@ -1,2 +0,0 @@
-function loadIcon(){$("#step1 img").attr("src","interface/assets/images/basket/icons/load.gif")}
-$(document).ready(function(){$("#back").click(function(){window.location="/basket"});$("input[rel='quickCheckout']").click(function(){$(".qcheckOff").hide();$(".qcheckOn").show();$("#siginTitle").html("Quick Checkout");$("#new_account").val(2)})})
